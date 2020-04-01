f37e46b41682a7d2c95508817a8ba18414be87a0
Mehmet Salih Bindak <mehmetsalihbindak@gmail.com>
2020-04-01 15:48:20 +0300

    top navigator

--- App/screens/DashboardScreen.js
+++ App/screens/DashboardScreen.js
@@ -1,13 +1,62 @@
 import React from 'react'
 import { StyleSheet } from 'react-native'
-import { Button, Input, Text, Layout } from '@ui-kitten/components';
+import {
+    Button, Input, Text, Layout, TopNavigation,
+    TopNavigationAction, Icon
+} from '@ui-kitten/components';
+
+
+
+const BackIcon = (style) => (
+    <Icon {...style} name='arrow-back' />
+);
+
+const EditIcon = (style) => (
+    <Icon {...style} name='edit' />
+);
+
+const MenuIcon = (style) => (
+    <Icon {...style} name='more-vertical' />
+);
+
+const BackAction = (props) => (
+    <TopNavigationAction {...props} icon={BackIcon} />
+);
+
+const EditAction = (props) => (
+    <TopNavigationAction {...props} icon={EditIcon} />
+);
+
+const MenuAction = (props) => (
+    <TopNavigationAction {...props} icon={MenuIcon} />
+);
+
 
 DashboardScreen = () => {
+    const onBackPress = () => {
+    };
 
+    const renderLeftControl = () => (
+        <BackAction onPress={onBackPress} />
+    );
+
+    const renderRightControls = () => [
+        <EditAction />,
+        <MenuAction />,
+    ];
     return (
-        <Layout style={styles.container}>
-            <Text>AT</Text>
-        </Layout>
+        <React.Fragment>
+            <Layout style={{ paddingTop: 20 }}>
+                <TopNavigation
+                    title='Application Title'
+                    leftControl={renderLeftControl()}
+                    rightControls={renderRightControls()}
+                />
+            </Layout>
+            <Layout style={styles.container}>
+                <Text>AT</Text>
+            </Layout>
+        </React.Fragment>
     )
 }
 
